81adec8edf75be809232556fe4465d2a89206a37
Hanie Sedghi <hsedghi@hsedghi-macbookpro2.roam.corp.google.com>
2018-07-02 16:24:24 -0700

    bcsv and bcsv_test added

---
+++ block_circulant_singular_values.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+import numpy as np
+
+def SVD_Conv_Tensor(conv, inp_shape):
+  """ Find the singular values of the linear transformation
+  corresponding to the convolution represented by conv on
+  an n x n x depth input. """
+  conv_tr = tf.cast(tf.transpose(conv, perm=[2, 3, 0, 1]), tf.complex64)
+  conv_shape = conv.get_shape().as_list()
+  padding = tf.constant([[0, 0], [0, 0],
+                         [0, inp_shape[0] - conv_shape[0]],
+                         [0, inp_shape[1] - conv_shape[1]]])
+  transform_coeff = tf.fft2d(tf.pad(conv_tr, padding))
+  singular_values = tf.svd(tf.transpose(transform_coeff, perm = [2, 3, 0, 1]),
+                           compute_uv=False)
+  return singular_values
+
+def Clip_OperatorNorm(conv, inp_shape, clip_to):
+  conv_tr = tf.cast(tf.transpose(conv, perm=[2, 3, 0, 1]), tf.complex64)
+  conv_shape = conv.get_shape().as_list()
+  padding = tf.constant([[0, 0], [0, 0],
+                         [0, inp_shape[0] - conv_shape[0]],
+                         [0, inp_shape[1] - conv_shape[1]]])
+  transform_coeff = tf.fft2d(tf.pad(conv_tr, padding))
+  D, U, V = tf.svd(tf.transpose(transform_coeff, perm = [2, 3, 0, 1]))
+  norm = tf.reduce_max(D)
+  D_clipped = tf.cast(tf.minimum(D, clip_to), tf.complex64)
+  clipped_coeff = tf.matmul(U, tf.matmul(tf.linalg.diag(D_clipped),
+                                         V, adjoint_b=True))
+  clipped_conv_padded = tf.real(tf.ifft2d(
+      tf.transpose(clipped_coeff, perm=[2, 3, 0, 1])))
+  return tf.slice(tf.transpose(clipped_conv_padded, perm=[2, 3, 0, 1]),
+                  [0] * len(conv_shape), conv_shape), norm
+
+def SVD_Conv_Tensor_NP(filter, inp_size):
+  # compute the singular values using FFT
+  # first compute the transforms for each pair of input and output channels
+  transform_coeff = np.fft.fft2(filter, inp_size, axes=[0, 1])
+
+  # now, for each transform coefficient, compute the singular values of the
+  # matrix obtained by selecting that coefficient for
+  # input-channel/output-channel pairs
+  return np.linalg.svd(transform_coeff, compute_uv=False)
+
+def Clip_OperatorNorm_NP(filter, inp_shape, clip_to):
+  # compute the singular values using FFT
+  # first compute the transforms for each pair of input and output channels
+  transform_coeff = np.fft.fft2(filter, inp_shape, axes=[0, 1])
+
+  # now, for each transform coefficient, compute the singular values of the
+  # matrix obtained by selecting that coefficient for
+  # input-channel/output-channel pairs
+  U, D, V = np.linalg.svd(transform_coeff, compute_uv=True, full_matrices=False)
+  D_clipped = np.minimum(D, clip_to)
+  if filter.shape[2] > filter.shape[3]:
+    clipped_transform_coeff = np.matmul(U, D_clipped[..., None] * V)
+  else:
+    clipped_transform_coeff = np.matmul(U * D_clipped[..., None, :], V)
+  clipped_filter = np.fft.ifft2(clipped_transform_coeff, axes=[0, 1]).real
+  args = [range(d) for d in filter.shape]
+  return clipped_filter[np.ix_(*args)]
+
+def Invert_Operator_NP(filter, inp_shape):
+  # computed the inverse of a filter. However the result is much bigger,
+  # (n x n) instead of (k x k), and we do not know a way to make it smaller.
+  # This is not so useful, as edge effects are now very important.
+
+  # compute the singular values using FFT
+  # first compute the transforms for each pair of input and output channels
+  transform_coeff = np.fft.fft2(filter, inp_shape, axes=[0, 1])
+
+  # now, for each transform coefficient, compute the singular values of the
+  # matrix obtained by selecting that coefficient for
+  # input-channel/output-channel pairs
+  U, D, V_t = np.linalg.svd(transform_coeff, compute_uv=True,
+                            full_matrices=False)
+  D_inv = 1.0 / (np.sign(D) * (np.absolute(D) + 1e-6))
+  U_t = np.transpose(U, (0, 1, 3, 2))
+  V = np.transpose(V_t, (0, 1, 3, 2))
+  if filter.shape[3] > filter.shape[2]:
+    inv_transform_coeff = np.matmul(V, D_inv[..., None] * U_t)
+  else:
+    inv_transform_coeff = np.matmul(V * D_inv[..., None, :], U_t)
+  inv_filter = np.fft.ifft2(inv_transform_coeff, axes=[0, 1]).real
+  return inv_filter
---
+++ block_circulant_singular_values_test.py
@@ -0,0 +1,218 @@
+import math
+import numpy as np
+import tensorflow as tf
+import time
+
+from tensorflow.python.platform import test
+import block_circulant_singular_values as bcsv
+
+class CirculantSingularTest(test.TestCase):
+
+  def testOneDimension(self):
+    """Test singular values of the one dimensional convolution."""
+
+    filter = np.array([2, 7, -1])
+    n = 20
+
+    # Compute the singular values directly
+    transform_mat = np.zeros([n, n])
+    for i in range(n):
+      for j in range(filter.size):
+        transform_mat[i, (j+i) % n] = filter[j]
+    U, D, V = np.linalg.svd(transform_mat)
+
+    # Compute the singular values using FFT
+    D1 = np.sort(np.absolute(np.fft.fft(filter, n)))[::-1]
+    self.assertAllCloseAccordingToType(D, D1)
+
+  def twoDimOneChannel(self, filter, n):
+    """Given a two-d, one-channel filter and an input size, return the matrix
+    of the linear transformation corresponding to the filter"""
+    transform_mat = np.zeros([n ** 2, n ** 2])
+    for i1 in range(n):
+      for i2 in range(n):
+        for j1 in range(filter.shape[0]):
+          for j2 in range(filter.shape[1]):
+            col = ((i1 + j1) % n) * n + (i2 + j2) % n
+            transform_mat[i1 * n + i2, col] = filter[j1, j2]
+    return transform_mat
+
+  def testTwoDimension(self):
+    """Test singular values of the two dimensional convolution."""
+    filter = np.array([[2, 7, -1], [1, 3, -8], [5, -3, 1]])
+    n = 20
+    # Compute the singular values directly
+    transform_mat = self.twoDimOneChannel(filter, n)
+    D = np.linalg.svd(transform_mat, compute_uv=False)
+
+    # Compute the singular values using FFT
+    D1 = np.sort(np.absolute(
+        np.fft.fft2(filter, [n, n]).flatten()))[::-1]
+    self.assertAllCloseAccordingToType(D, D1)
+
+  def testTwoDimensionRectangular(self):
+    """Test singular values of the two dimensional convolution."""
+    filter = np.array([[2, 7, -1], [1, 3, -8]])
+    n = 20
+    # Compute the singular values directly
+    transform_mat = self.twoDimOneChannel(filter, n)
+    D = np.linalg.svd(transform_mat, compute_uv=False)
+
+    # Compute the singular values using FFT
+    D1 = np.sort(np.absolute(
+        np.fft.fft2(filter, [n, n]).flatten()))[::-1]
+    self.assertAllCloseAccordingToType(D, D1)
+
+  def testMultiChannel(self):
+    """Test the case where inputs and outputs have several channels each"""
+
+    num_inp_channels = 2
+    num_out_channels = 3
+    filter_x = 3
+    filter_y = 4
+    filter_shape = (filter_x, filter_y, num_inp_channels, num_out_channels)
+    filter = np.random.randint(low=-8, high=8,size=filter_shape)
+    n = 32
+    # Compute the singular values directly
+    print("Start Full Matrix")
+    start = time.time()
+    transform_mat = np.zeros([num_inp_channels * (n ** 2),
+                              num_out_channels * (n ** 2)])
+    for c1 in range(num_inp_channels):
+      for c2 in range(num_out_channels):
+        first_row = n * n * c1
+        first_col = n * n * c2
+        this_block = self.twoDimOneChannel(filter[:,:,c1,c2], n)
+        transform_mat[first_row:(first_row+n*n),
+                      first_col:(first_col+n*n)] = this_block
+
+    D = np.linalg.svd(transform_mat, compute_uv=False)
+    print("Time for SVD Full Matrix:", time.time() - start)
+
+    start = time.time()
+    singular_vals_by_freq_pair = bcsv.SVD_Conv_Tensor_NP(filter, [n, n])
+    print("Short algorithm time:", time.time() - start)
+
+    # sort singular values in decreasing order
+    D1 = np.flip(np.sort(singular_vals_by_freq_pair.flatten()),0)
+    self.assertAllCloseAccordingToType(D, D1)
+
+  def testMultiChannelTF(self):
+    """Test the case where inputs and outputs have several channels each.
+    Using much bigger input, to check timing."""
+
+    num_inp_channels = 64
+    num_out_channels = 256
+    filter_x = 3
+    filter_y = 4
+    filter_shape = (filter_x, filter_y, num_inp_channels, num_out_channels)
+    filter = np.random.randint(low=-8, high=8,size=filter_shape)
+    n = 32
+
+    start = time.time()
+    singular_vals_by_freq_pair = bcsv.SVD_Conv_Tensor_NP(filter, [n, n])
+
+    print("NP SVD time:", time.time() - start)
+    # sort singular values in decreasing order
+    D1 = np.flip(np.sort(singular_vals_by_freq_pair.flatten()),0)
+
+    with self.test_session() as sess:
+      filter_tf = tf.constant(filter, dtype=tf.float32)
+      D2_tf = bcsv.SVD_Conv_Tensor(filter_tf, [n, n])
+
+      tf.global_variables_initializer().run()
+      start = time.time()
+      singular_vals = sess.run(D2_tf)
+      print("TF SVD Time:", time.time() - start)
+      D2 = np.flip(np.sort(singular_vals.flatten()),0)
+      self.assertAllClose(D1 / D1[0], D2 / D2[0], atol=3e-5)
+
+
+  def testMultiChannelClipRepeated(self):
+    print("Testing Repeated Clipping")
+    num_inp_channels = 3
+    num_out_channels = 4
+    filter_x = 3
+    filter_y = 4
+    filter_shape = (filter_x, filter_y, num_inp_channels, num_out_channels)
+    filter = np.random.randint(low=-8, high=8,size=filter_shape)
+    n = 32
+    singular_vals = bcsv.SVD_Conv_Tensor_NP(filter, [n, n])
+    clipped_filter = bcsv.Clip_OperatorNorm_NP(filter, [n, n],
+                                               singular_vals.max())
+    self.assertAllClose(filter, clipped_filter)
+
+    clip_value = 10
+    last_max = singular_vals.max()
+    for round in range(10):
+      clipped_filter = bcsv.Clip_OperatorNorm_NP(clipped_filter, [n, n],
+                                                 clip_value)
+      clipped_singular_vals = bcsv.SVD_Conv_Tensor_NP(clipped_filter, [n, n])
+      self.assertTrue(last_max > clipped_singular_vals.max())
+      last_max = clipped_singular_vals.max()
+
+  def testMultiChannelClipTF(self):
+    print("Testing Clipping TF vs Numpy")
+    num_inp_channels = 32
+    num_out_channels = 64
+    filter_x = 3
+    filter_y = 4
+    filter_shape = (filter_x, filter_y, num_inp_channels, num_out_channels)
+    filter = np.random.randint(low=-8, high=8,size=filter_shape)
+    n = 32
+    start = time.time()
+    clipped_filter = bcsv.Clip_OperatorNorm_NP(filter, [n, n], 10)
+    print("Numpy Clipping Time:", time.time() - start)
+
+    with self.test_session() as sess:
+      filter_tf = tf.constant(filter, dtype=tf.float32)
+      clipped_filter_tf, norm = bcsv.Clip_OperatorNorm(filter_tf, [n, n], 10)
+
+      tf.global_variables_initializer().run()
+      start = time.time()
+      clipped_filter2 = sess.run(clipped_filter_tf)
+      print("TF Clipping Time:", time.time() - start)
+      self.assertAllClose(clipped_filter2, clipped_filter, atol=3e-5)
+
+
+  def testInverse(self):
+    num_inp_channels = 2
+    num_out_channels = 3
+    filter_x = 3
+    filter_y = 4
+    filter_shape = (filter_x, filter_y, num_inp_channels, num_out_channels)
+    filter = np.random.randint(low=-8, high=8, size=filter_shape)
+    n = 16
+    inverse_filter = bcsv.Invert_Operator_NP(filter, [n, n])
+    inverse_inverse_filter = bcsv.Invert_Operator_NP(inverse_filter, [n, n])
+    #print filter
+    #print inverse_filter
+    #print inverse_inverse_filter
+    args = [range(d) for d in filter_shape]
+    proj_filter = inverse_inverse_filter[np.ix_(*args)]
+    self.assertAllClose(filter, proj_filter, atol=3e-4)
+
+  """
+  def testStrides(self):
+    filter = np.array([2, 7, -1, 16])
+    n = 30
+    stride = 2
+    # Compute the singular values directly
+    transform_mat = np.zeros([n, n])
+    for i in range(n):
+      for j in range(filter.size):
+        transform_mat[i, (j+i*stride) % n] = filter[j]
+    U, D, V = np.linalg.svd(transform_mat)
+    print D
+    print np.linalg.eig(transform_mat)
+
+    transform_mat = np.zeros([n/stride, n])
+    for i in range(n/stride):
+      for j in range(filter.size):
+        transform_mat[i, (j+i*stride) % n] = filter[j]
+    U, D, V = np.linalg.svd(transform_mat)
+    print D * math.sqrt(stride)
+  """
+
+if __name__ == '__main__':
+  test.main()
